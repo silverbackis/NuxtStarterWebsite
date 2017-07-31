04aedf1e217cce731c75701824dece99b857611d
Daniel West <daniel@silverback.is>
2017-07-31 11:39:46 +0100

    Stripped back - showing bug with less interference

--- pages/_page/index.vue
+++ pages/_page/index.vue
@@ -1,39 +1,25 @@
 <template>
     <section>
-        <!-- <hero :title="page.header" :subtitle="page.subHeader" :patchUrl="page.patchUrl" />
-        <section class="section">
-            <div class="container">
-                <div class="columns"> -->
-                    <div v-if="page.hasChildren" class="column is-narrow">
-                        <side-menu :sideMenu="page.sideMenu" :topLabel="page.subLabel" addUrl="/admin/news" v-on:reloadSideMenu="reloadSideMenu" />
-                    </div>
-                    <div class="column column-content">
-                        <nuxt-child :key="routerViewKey"
-                                    :nuxtParent="page"
-                                    v-on:reloadSideMenu="reloadSideMenu"
-                        />
-                    </div>
-        <!-- </div>
-    </div>
-</section> -->
+      <div v-if="page.hasChildren" class="column is-narrow">
+        <side-menu :sideMenu="page.sideMenu" :topLabel="page.subLabel" addUrl="/admin/news" v-on:reloadSideMenu="reloadSideMenu" />
+      </div>
+      <div class="column column-content">
+        <nuxt-child :key="$route.fullPath"
+                    :nuxtParent="page"
+                    v-on:reloadSideMenu="reloadSideMenu"
+        />
+      </div>
     </section>
 </template>
 
 <script>
-  import Hero from '~/components/Template/Hero.vue'
   import SideMenu from '~/components/SideMenu/SideMenu.vue'
   import PageMixin from '~/plugins/ApiPageMixin'
 
   export default {
     mixins: [PageMixin],
     components: {
-      Hero,
       SideMenu
-    },
-    computed: {
-      routerViewKey () {
-        return this.$route.fullPath
-      }
     },
     asyncData ({ app, params }) {
       return app.$getPage({
